04cce8358ec12ce4629beed667e4e63580da289f
aga555 <58139699+aga555@users.noreply.github.com>
2019-11-25 16:45:23 +0100

    workshop
    work

--- chapter-2-end/src/main.js
+++ chapter-2-end/src/main.js
@@ -1,25 +1,34 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import vuetify from '@/plugins/vuetify'; // path to vuetify export
-import 'vuetify/dist/vuetify.min.css';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import vuetify from "@/plugins/vuetify"; // path to vuetify export
+import "vuetify/dist/vuetify.min.css";
 
-import App from './App';
-import Home from './views/Home';
-import Pets from './views/Pets';
+import App from "./App";
+import Home from "./views/Home";
+import Pets from "./views/Pets";
+import Favorites from "./views/Favorites";
+import store from "./store/store";
+import Form from "./views/Form";
 
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
-const routes = [{ path: '/', component: Home }, { path: '/pets', component: Pets }];
+const routes = [
+  { path: "/", component: Home },
+  { path: "/pets", component: Pets },
+  { path: "/favorites", component: Favorites },
+  { path: "/form", component: Form }
+];
 
 const router = new VueRouter({ routes });
 
 /* eslint-disable no-new */
 new Vue({
   vuetify,
-  el: '#app',
-  components: { App },
-  template: '<App/>',
   router,
+  store,
+  el: "#app",
+  components: { App },
+  template: "<App/>"
 });
